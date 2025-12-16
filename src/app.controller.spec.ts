4a517c76477e40f0d392f478c4a6d8a920d3b892
Siona Mariam Thomas <siona.thomas@reflectionsinfos.com>
2025-12-16 22:55:22 +0530

    to create merge

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('this is a small complicated code foebw shdhsjsbdjbsjbj"', () => {
+      expect(appController.getUser()).toBe('Hello World!');
     });
   });
 });
